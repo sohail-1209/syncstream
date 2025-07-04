459bdc90463a3536807231d36b0a7df13304b01b
Sohail <synora.nexi@gmail.com>
2025-07-04 15:55:53 +0000

    same error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#a855f7" />
+        <meta name="theme-color" content="#4B0082" />
       </head>
       <body className="font-body antialiased">
         {children}
